a4c5c6b0360052d0a73eeab3316d18d75f48fc7c
Drapiciuc Daniel <126782377+JustCozMonkey@users.noreply.github.com>
2025-04-30 11:29:51 +0300

    1st page

--- posts.js
+++ posts.js
@@ -1,36 +1,36 @@
 export const posts = [
     {
-        img: "imgs/img1.png",
+        img: "/imgs/img1.png",
         date: "MAY 29 2025",
         blog: "Blog one",
         text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
     },
     {
-        img: "imgs/img2.png",
+        img: "/imgs/img2.png",
         date: "MAY 29 2025",
         blog: "Blog two",
         text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
     },
     {
-        img: "imgs/img3.png",
+        img: "/imgs/img3.png",
         date: "MAY 29 2025",
         blog: "Blog three",
         text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
     },
     {
-        img: "imgs/img4.png",
+        img: "/imgs/img4.png",
         date: "MAY 29 2025",
         blog: "Blog four",
         text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
     },
     {
-        img: "imgs/img5.png",
+        img: "/imgs/img5.png",
         date: "MAY 29 2025",
         blog: "Blog five",
         text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
     },
     {
-        img: "imgs/img6.png",
+        img: "/imgs/img6.png",
         date: "MAY 29 2025",
         blog: "Blog six",
         text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
